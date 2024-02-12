7f9c7f12785a0c2e13023db301cebf72451ce472
Jamie Socorro <jamie_socorro@live.com>
2024-02-12 19:34:08 +0800

    feat: updated descriptions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AboutComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HeaderComponent } from 'src/app/shared/components/header/header.component';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { SafePipe } from 'src/assets/pipe/safeUrl.pipe';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  standalone: true,
+  imports: [HeaderComponent, SafePipe],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  trustedDashboardUrl: SafeUrl;
+  constructor(private _sanitizer: DomSanitizer) {
+    this.trustedDashboardUrl = this._sanitizer.bypassSecurityTrustResourceUrl(
+      'https://www.youtube.com/watch?v=8-zQKcxXm7w'
+    );
   }
 
+  ngOnInit(): void {}
+
+  getUrl(url: string) {
+    return this._sanitizer.bypassSecurityTrustResourceUrl(
+      'https://' + url + '/'
+    );
+  }
 }
